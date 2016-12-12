275ce7e9102cc9f8fd1dc20b26bf0267c980df88
Jane Geryak <janegeryak@gmail.com>
2016-12-12 12:43:27 +0200

    grammar mistakes

--- script.js
+++ script.js
@@ -20,29 +20,29 @@ urlArray [4] = "https://www.instagram.com/jenny_geryak/"
 
 mnuArray = new Array()
 mnuArray [0] = "Site"
-mnuArray [1] = "Githud"
+mnuArray [1] = "Github"
 mnuArray [2] = "Codepen"
 mnuArray [3] = "Linkedin"
 mnuArray [4] = "Instagram"
-document.write()
-if (document.title == titArray[0])
-{document.write(div1+mnuArray[0]+div2)}
-else
-{document.write(div1+lnk1+urlArray[0]+lnk2+mnuArray[0]+lnk3+div2)}
-if (document.title == titArray[1])
-{document.write(div1+mnuArray[1]+div2)}
-else
-{document.write(div1+lnk1+urlArray[1]+lnk2+mnuArray[1]+lnk3+div2)}
-if (document.title == titArray[2])
-{document.write(div1+mnuArray[2]+div2)}
-else
-{document.write(div1+lnk1+urlArray[2]+lnk2+mnuArray[2]+lnk3+div2)}
-if (document.title == titArray[3])
-{document.write(div1+mnuArray[3]+div2)}
-else
-{document.write(div1+lnk1+urlArray[3]+lnk2+mnuArray[3]+lnk3+div2)}
-if (document.title == titArray[4])
-{document.write(div1+mnuArray[4]+div2)}
-else
-{document.write(div1+lnk1+urlArray[4]+lnk2+mnuArray[4]+lnk3+div2)}
-document.write("</tr></table>")
+	document.write()
+		if (document.title == titArray[0])
+	{document.write(div1+mnuArray[0]+div2)}
+		else
+	{document.write(div1+lnk1+urlArray[0]+lnk2+mnuArray[0]+lnk3+div2)}
+		if (document.title == titArray[1])
+	{document.write(div1+mnuArray[1]+div2)}
+		else
+	{document.write(div1+lnk1+urlArray[1]+lnk2+mnuArray[1]+lnk3+div2)}
+		if (document.title == titArray[2])
+	{document.write(div1+mnuArray[2]+div2)}
+		else
+	{document.write(div1+lnk1+urlArray[2]+lnk2+mnuArray[2]+lnk3+div2)}
+		if (document.title == titArray[3])
+	{document.write(div1+mnuArray[3]+div2)}
+		else
+	{document.write(div1+lnk1+urlArray[3]+lnk2+mnuArray[3]+lnk3+div2)}
+		if (document.title == titArray[4])
+	{document.write(div1+mnuArray[4]+div2)}
+		else
+	{document.write(div1+lnk1+urlArray[4]+lnk2+mnuArray[4]+lnk3+div2)}
+
