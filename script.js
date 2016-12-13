29171757cbd6b756af039a2bf7cb18b60b13c7b8
Jane Geryak <janegeryak@gmail.com>
2016-12-13 19:59:46 +0200

    menu structure

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-var div1 = "<td><div class='txtmenu'>"
-var lnk1 = "<a href='"
-var lnk2 = "' class='lnkmenu'>"
-var lnk3 = "</a>"
-var div2 = "</div></td>"
+var newarray=[
+	["Site", "https://jennygeryak.github.io/"],
+	["Github", "https://github.com/JennyGeryak"],
+	["Codepen", "http://codepen.io/JennyGeryak/"],
+	["Linkedin", "https://www.linkedin.com/in/jane-geryak-721719a5"],
+	["Instagram", "https://www.instagram.com/jenny_geryak/"]
+];
+var menu_menu=document.getElementById("menu");
+var menu = document.createElement("nav");
+menu_menu.appendChild(menu);
+var menu_body = document.createElement("ul");
 
-titArray = new Array()
-titArray [0] = "Site"
-titArray [1] = "Github"
-titArray [2] = "Codepen"
-titArray [3] = "Linkedin"
-titArray [4] = "Instagram"
+for (var i = 0;i<newarray.length; i=i+1) {
+	console.dir(newarray[i]);
+	menu.appendChild(menu_body);
+	var element_container = document.createElement("li");
+	menu_body.appendChild(element_container);
+	var element = document.createElement("a");
+	element_container.appendChild(element);
+}
+ 
 
-urlArray = new Array()
-urlArray [0] = "https://jennygeryak.github.io/"
-urlArray [1] = "https://github.com/JennyGeryak"
-urlArray [2] = "http://codepen.io/JennyGeryak/"
-urlArray [3] = "https://www.linkedin.com/in/jane-geryak-721719a5"
-urlArray [4] = "https://www.instagram.com/jenny_geryak/"
-
-mnuArray = new Array()
-mnuArray [0] = "Site"
-mnuArray [1] = "Github"
-mnuArray [2] = "Codepen"
-mnuArray [3] = "Linkedin"
-mnuArray [4] = "Instagram"
-	document.write()
-		if (document.title == titArray[0])
-	{document.write(div1+mnuArray[0]+div2)}
-		else
-	{document.write(div1+lnk1+urlArray[0]+lnk2+mnuArray[0]+lnk3+div2)}
-		if (document.title == titArray[1])
-	{document.write(div1+mnuArray[1]+div2)}
-		else
-	{document.write(div1+lnk1+urlArray[1]+lnk2+mnuArray[1]+lnk3+div2)}
-		if (document.title == titArray[2])
-	{document.write(div1+mnuArray[2]+div2)}
-		else
-	{document.write(div1+lnk1+urlArray[2]+lnk2+mnuArray[2]+lnk3+div2)}
-		if (document.title == titArray[3])
-	{document.write(div1+mnuArray[3]+div2)}
-		else
-	{document.write(div1+lnk1+urlArray[3]+lnk2+mnuArray[3]+lnk3+div2)}
-		if (document.title == titArray[4])
-	{document.write(div1+mnuArray[4]+div2)}
-		else
-	{document.write(div1+lnk1+urlArray[4]+lnk2+mnuArray[4]+lnk3+div2)}
 
