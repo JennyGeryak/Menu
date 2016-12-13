601b584742f18fc10571b0a4b32be56a940b03f6
Jane Geryak <janegeryak@gmail.com>
2016-12-13 20:42:41 +0200

    codepen menu

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var newarray=[
 	["Site", "https://jennygeryak.github.io/"],
 	["Github", "https://github.com/JennyGeryak"],
-	["Codepen", "http://codepen.io/JennyGeryak/"],
+	["Codepen", "http://codepen.io/JennyGeryak/",[
+		["HTML-form", "http://s.codepen.io/JennyGeryak/debug/qqXrBJ"],
+		["Running_button", "http://s.codepen.io/JennyGeryak/debug/RoZpgN"],
+		["Check_list", "http://s.codepen.io/JennyGeryak/debug/yVoMpv"]
+	]],
 	["Linkedin", "https://www.linkedin.com/in/jane-geryak-721719a5"],
 	["Instagram", "https://www.instagram.com/jenny_geryak/"]
 ];
@@ -16,7 +20,10 @@ for (var i = 0;i<newarray.length; i=i+1) {
 	var element_container = document.createElement("li");
 	menu_body.appendChild(element_container);
 	var element = document.createElement("a");
+	console.dir(element);
 	element_container.appendChild(element);
+	element.innerHTML=newarray[i][0];
+	element.href=newarray[i][1];
 }
  
 
