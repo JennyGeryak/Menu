2ecb311022f5d20cf8945573ff7ff3bc2deddf20
Jane Geryak <janegeryak@gmail.com>
2016-12-19 17:49:55 +0200

    menu script

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var newarray=[
 	["Codepen", "http://codepen.io/JennyGeryak/",[
 		["HTML-form", "http://s.codepen.io/JennyGeryak/debug/qqXrBJ"],
 		["Running_button", "http://s.codepen.io/JennyGeryak/debug/RoZpgN"],
-		["Check_list", "http://s.codepen.io/JennyGeryak/debug/yVoMpv"]
+		["Check_list", "http://s.codepen.io/JennyGeryak/debug/yVoMpv",]
 	]],
 	["Linkedin", "https://www.linkedin.com/in/jane-geryak-721719a5"],
 	["Instagram", "https://www.instagram.com/jenny_geryak/"]
@@ -12,19 +12,24 @@ var newarray=[
 var menu_menu=document.getElementById("menu");
 var menu = document.createElement("nav");
 menu_menu.appendChild(menu);
-var menu_body = document.createElement("ul");
 
-for (var i = 0;i<newarray.length; i=i+1) {
-	console.dir(newarray[i]);
-	menu.appendChild(menu_body);
-	var element_container = document.createElement("li");
-	menu_body.appendChild(element_container);
-	var element = document.createElement("a");
-	console.dir(element);
-	element_container.appendChild(element);
-	element.innerHTML=newarray[i][0];
-	element.href=newarray[i][1];
+function Menu (array, parent) {
+	var menu_body = document.createElement("ul");
+	parent.appendChild(menu_body);
+	for (var i = 0;i<array.length; i=i+1){
+		var element_container = document.createElement("li");
+		menu_body.appendChild(element_container);
+		var element = document.createElement("a");
+		element_container.appendChild(element);
+		element.innerHTML=array[i][0];
+		element.href=array[i][1];	
+		if (array[i].length==3) {
+			Menu(array[i][2], element_container);
+		}
+	}
+
 }
- 
+Menu (newarray, menu);
+
 
 
